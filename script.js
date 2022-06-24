5e540f6c985d28d4a7ec61c789c56a04ef388d29
Reza <privyreza@gmail.com>
2022-06-24 21:13:15 +0200

    Initial COmmit

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+    return parseInt(num1) + parseInt(num2);
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num1;
+}
+
+// Test
+add(1, 4);
